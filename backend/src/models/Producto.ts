8364ae3c054cc33fda1356a4a1995368cc708428
Jorge Casteleiro <casteleirojorge@gmail.com>
2026-07-09 22:15:59 +0200

    update: modelo producto con moneda

--- backend/src/models/Producto.ts
+++ backend/src/models/Producto.ts
@@ -3,21 +3,35 @@ import mongoose, { Schema, Document } from 'mongoose';
 export interface IProducto extends Document {
   nombre: string;
   precio: number;
-  receta?: {
+  moneda: 'CUP' | 'USD'; // NUEVO: Moneda en la que se vende el plato al cliente
+  receta: {
     ingrediente: mongoose.Schema.Types.ObjectId;
     cantidad: number;
   }[];
 }
 
 const ProductoSchema: Schema = new Schema({
-  nombre: { type: String, required: true },
-  precio: { type: Number, required: true },
+  nombre: { 
+    type: String, 
+    required: true,
+    unique: true // Evita que registres dos veces el mismo plato
+  },
+  precio: { 
+    type: Number, 
+    required: true 
+  },
+  moneda: {
+    type: String,
+    enum: ['CUP', 'USD'],
+    default: 'CUP', // Por defecto lo ponemos en CUP
+    required: true
+  },
   receta: [
     {
-      ingrediente: { type: mongoose.Schema.Types.ObjectId, ref: 'Ingrediente' },
+      ingrediente: { type: mongoose.Schema.Types.ObjectId, ref: 'Ingrediente', required: true },
       cantidad: { type: Number, required: true }
     }
   ]
-});
+}, { timestamps: true });
 
 export default mongoose.model<IProducto>('Producto', ProductoSchema);
